0f6885faa17ec319b9d69872c892c439bf48da4e
unknown <El_Bald@aucegypt.edu>
2026-04-13 15:42:19 +0200

    Refactored data memeory to support byte and half-word addressing

--- riscv full single cycle implementation.srcs/sources_1/new/data_memory.v
+++ riscv full single cycle implementation.srcs/sources_1/new/data_memory.v
@@ -19,9 +19,16 @@ module data_memory (
     // Control signals
     input wire mem_read,
     input wire mem_write,
+   
+    //signed extnsion or unsigned etension
+    input wire signed_ex,
     
+    
+    // byte, halfword, or word
+    input wire [1:0] size,
+   
     // Address
-    input wire [5:0] address,
+    input wire [7:0] address,
     
     // Data
     input wire [31:0] write_data,
@@ -31,9 +38,63 @@ module data_memory (
     // Data RAM (64 x 32-bit)
     reg [31:0] data [0:63];
     
-    // Read logic with zero output when not reading
-    assign read_data = mem_read ? data[address] : `ZERO_32_BIT;
-
+    always @(*) begin
+    if(mem_read == 1'b0) read_data = `ZERO_32_BIT;
+    else begin
+        case (size)
+        2'b00: begin // Load Byte(LB)
+                    if(signed_ex == 1'b1) // if signed byte, copy sign bit
+                        if(address[1:0] == 2'b00)
+                            read_data = {{24{address[31:2][7]}},data[address[31:2]][7:0]};
+                        else if(address[1:0] == 2'b01)
+                            read_data = {{24{address[31:2][15]}},data[address[31:2]][15:8]};
+                        else if(address[1:0] == 2'b10)
+                            read_data = {{24{address[31:2][23]}},data[address[31:2]][23:16]};
+                        else 
+                            read_data = {{24{address[31:2][31]}},data[address[31:2]][31:24]};
+                        
+                    else // otherwise, copy zeroes
+                        if(address[1:0] == 2'b00)
+                            read_data = {{24{0}},data[address[31:2]][7:0]};
+                        else if(address[1:0] == 2'b01)
+                            read_data = {{24{0}},data[address[31:2]][15:8]};
+                        else if(address[1:0] == 2'b10)
+                            read_data = {{24{0}},data[address[31:2]][23:16]};
+                        else 
+                            read_data = {{24{0}},data[address[31:2]][31:24]};
+            end
+            2'b01: begin // Load Half Word(LH)
+                // Only allow read if address is halfword-aligned (ends in 0)
+                if (address[0] == 1'b0) begin
+                    if(signed_ex == 1'b1) // if signed halfword, copy sign bit
+                        if(address[1] == 1'b1)
+                            read_data = {{16{address[31:2][31]}},data[address[31:2]][31:16]};
+                        else
+                            read_data = {{16{address[31:2][15]}},data[address[31:2]][15:0]};
+                    else // otherwise, copy zeroes
+                        if(address[1] == 1'b1)
+                            read_data = {{16{0}},data[address[31:2]][31:16]};
+                        else
+                            read_data = {{16{0}},data[address[31:2]][15:0]};
+                end 
+                else begin
+                    read_data = `ZERO_32_BIT; // or trigger an alignment fault signal, but we will just put zeroes
+                end
+            end
+            2'b10: begin // Load Word (LW)
+                // Only allow read if address is word-aligned (ends in 00)
+                if (address[1:0] == 2'b00) begin
+                    read_data = data[address[31:2]]; 
+                end else begin
+                    read_data = `ZERO_32_BIT; // or trigger an alignment fault signal, but we will just put zeroes
+                end
+            end
+           default: read_data = `ZERO_32_BIT;
+        endcase
+    
+       end     
+    end
+    
     // Test data initialization 
     initial begin
         data[0]=32'd17;
@@ -44,7 +105,27 @@ module data_memory (
     // Write logic
     always @(posedge clk) begin
         if (mem_write) begin
-            data[address] <= write_data;
+            case(size)
+                2'b00: begin
+                    if(address[1:0] == 2'b00)
+                        data[address[31:2]][7:0] <= write_data[7:0];
+                    else if(address[1:0] == 2'b01)
+                        data[address[31:2]][15:8] <= write_data[7:0];
+                    else if(address[1:0] == 2'b10)
+                        data[address[31:2]][23:16] <= write_data[7:0];
+                    else 
+                        data[address[31:2]][31:24] <= write_data[7:0];
+                end
+                2'b01: begin
+                    if(address[1] == 1'b1)
+                        data[address[31:2]][31:16] <= write_data[15:0];
+                    else
+                        data[address[31:2]][15:0] <= write_data[15:0];
+                end
+                2'b10: begin
+                    data[address[31:2]] <= write_data;
+                end
+            endcase
         end
     end
 
